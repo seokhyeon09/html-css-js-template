6acce5b2c2d0fefc6e6d0902e54461846a1d8df1
seokhyeon09 <ring4392@gmail.com>
2026-01-28 12:36:09 +0900

    Swiper Update

--- js/app.js
+++ js/app.js
@@ -1,27 +1,42 @@
 // alert("hello javascript")
 
-const body =document.querySelector('body')
-const header =document.querySelector('header')
-const upBtn =document.querySelector('.up-btn')
-const mobNavBtn =document.querySelector('.mob-nav-btn')
+const body = document.querySelector('body')
+const header = document.querySelector('header')
+const upBtn = document.querySelector('.up-btn')
+const mobNavBtn = document.querySelector('.mob-nav-btn')
 // 복수 선택형일때 사용
-const navList =document.querySelectorAll('.nav-list li')
+const navList = document.querySelectorAll('.nav-list li')
 
+
+// sec-1 슬라이드 스크립트
+var galleryswiper = new Swiper(".gallery", {
+    pagination: {
+        el: ".gallery .swiper-pagination",
+    },
+    navigation: {
+        nextEl: ".gallery .swiper-button-next",
+        prevEl: ".gallery .swiper-button-prev",
+    },
+    // 반복허용
+    loop:true,
+    // 자동움직임
+    // autoplay:true,
+});
 
 // 매개변수, 순번
-navList.forEach((btn, i)=>{
-    btn.addEventListener('click', (e)=>{
+navList.forEach((btn, i) => {
+    btn.addEventListener('click', (e) => {
         //부드럽게 이동 윗줄의 ()안에 e도 들어가야함
         e.preventDefault()
-        console.log(document.querySelector(`#sec-${i+1}`))
-        document.querySelector(`#sec-${i+1}`).scrollIntoView({
-            behavior:'smooth'
-        })  
+        console.log(document.querySelector(`#sec-${i + 1}`))
+        document.querySelector(`#sec-${i + 1}`).scrollIntoView({
+            behavior: 'smooth'
+        })
     })
 })
 
 // classList.toggle()
-mobNavBtn.addEventListener('click', (e)=>{
+mobNavBtn.addEventListener('click', (e) => {
     e.preventDefault()
     body.classList.toggle('mob-nav-open')
 })
@@ -29,20 +44,20 @@ mobNavBtn.addEventListener('click', (e)=>{
 
 // console.log(fixedArrow)
 
-window.addEventListener('scroll',()=>{
-    
-    if(window.scrollY>=10){
+window.addEventListener('scroll', () => {
+
+    if (window.scrollY >= 10) {
         body.classList.add('scroll')
-    }else{
+    } else {
         body.classList.remove('scroll')
     }
 })
 
 
-upBtn.addEventListener('click',()=>{
+upBtn.addEventListener('click', () => {
     window.scrollTo({
-        top:0,
-        behavior:'smooth'
+        top: 0,
+        behavior: 'smooth'
     })
 })
 
@@ -52,21 +67,21 @@ const contentList = document.querySelectorAll(".content");
 
 let i = 0;
 init(i);
-function init(x){
+function init(x) {
     menuList[x].classList.add("active");
     contentList[x].classList.add("on");
 }
-function rest(){
-    for(let i=0; i<menuList.length; i++){
-            menuList[i].classList.remove("active");
-            contentList[i].classList.remove("on");
+function rest() {
+    for (let i = 0; i < menuList.length; i++) {
+        menuList[i].classList.remove("active");
+        contentList[i].classList.remove("on");
     }
 }
-menuList.forEach((menu,index)=>{
-    menu.addEventListener("click",(e)=>{
+menuList.forEach((menu, index) => {
+    menu.addEventListener("click", (e) => {
         e.preventDefault();
         console.log(menu, index);
         rest();
         init(index);
     });
-}); 
+});
